eb04b820da82cec4a654b7642cf1366fe830ba3a
md.mahmudur.rahman <md.mahmudur.rahman.bd@gmail.com>
2017-12-15 08:05:59 +0700

    Change params for env

--- app/controllers/browserlog/logs_controller.rb
+++ app/controllers/browserlog/logs_controller.rb
@@ -1,17 +1,18 @@
 module Browserlog
   class LogsController < ActionController::Base
-    # before_filter :check_env
+    before_filter :check_env
     before_action :check_auth
+    before_action :current_environment
 
     layout 'browserlog/application'
 
     def index
-      @filename = "#{params[:env]}.log"
+      @filename = "#{@env}.log"
       @filepath = Rails.root.join("log/#{@filename}")
     end
 
     def changes
-      lines, last_line_number = reader.read(offset: params[:currentLine].to_i, log_file_name: params[:env])
+      lines, last_line_number = reader.read(offset: params[:currentLine].to_i, log_file_name: @env)
 
       respond_to do |format|
         format.json do
@@ -34,11 +35,15 @@ module Browserlog
     end
 
     def check_env
-      raise unless Browserlog.config.allowed_log_files.include?(params[:env])
+      raise unless Browserlog.config.allowed_log_files.include?(@env)
     end
 
     def check_auth
       raise 'Logs not allowed on production environment.' if Rails.env.production? && !Browserlog.config.allow_production_logs
+    end
+
+    def current_environment
+      @env = Rails.env
     end
   end
 end
