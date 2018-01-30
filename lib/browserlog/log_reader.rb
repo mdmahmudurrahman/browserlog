2cc49f731a3e3fbff34f074325171e3a6f2f2b47
md.mahmudur.rahman <md.mahmudur.rahman.bd@gmail.com>
2018-01-30 13:07:41 +0700

    Change log reader

--- lib/browserlog/log_reader.rb
+++ lib/browserlog/log_reader.rb
@@ -1,28 +1,26 @@
 module Browserlog
   class LogReader
+    # def read(options = {})
+    #   @log_file_name = options[:log_file_name] || 'development'
+    #   offset = options[:offset] || -1
+    #   limit = options[:limit] || 25
+    #   amount = [limit, remaining_lines(offset)].min
+
+    #   line_index = offset == -1 ? num_lines : offset + amount
+
+    #   [readlines(amount), line_index]
+    # end
+
     def read(options = {})
       @log_file_name = options[:log_file_name] || 'development'
-      offset = options[:offset] || -1
-      limit = options[:limit] || 25
-      amount = [limit, remaining_lines(offset)].min
-
-      line_index = offset == -1 ? num_lines : offset + amount
-
-      [readlines(amount), line_index]
+      limit = 25
+      [readlines(limit), limit]
     end
 
     private
 
-    def remaining_lines(offset)
-      offset == -1 ? num_lines : (num_lines - offset)
-    end
-
     def log_path
       Rails.root.join("log/#{@log_file_name}.log")
-    end
-
-    def num_lines
-      `wc -l #{log_path}`.split.first.to_i
     end
 
     def readlines(amount)
